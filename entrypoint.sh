7f4e1641e596b66d8537aaaf5c14f5c17fc5e611
jadutter <4691511+jadutter@users.noreply.github.com>
2021-02-15 00:02:22 -0500

    reverted entrypoint to see if pipe redirections are breaking the flow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# print the output and errors, but also send them to files to be read later
-((eval $INPUT_CMD | tee /tmp/output) 3>&1 1>&2 2>&3 | tee /tmp/error)
+eval $INPUT_CMD > /tmp/output > /tmp/error
 EXIT_CODE="$?"
 echo "::set-output name=stderr::$(cat /tmp/error)"
 echo "::set-output name=stdout::$(cat /tmp/output)"

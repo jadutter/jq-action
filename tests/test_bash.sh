445b824cfbeeb3a505e248ce25d9874cc8b8ff38
jadutter <4691511+jadutter@users.noreply.github.com>
2021-02-22 18:21:00 -0500

    updated development workflow to match specific values and throw errors

--- tests/test_bash.sh
+++ tests/test_bash.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 function test_bash(){
-    echo '{"foo":"bar"}' | 
-    jq 'keys'
-
 	local get_values="$(cat<<EOF
 def values:
 	to_entries |
@@ -16,4 +13,4 @@ EOF
 )"
     jq "${get_values}" ./tests/sample.json
 }
-test_bash 
+test_bash | jq -sc 'add'

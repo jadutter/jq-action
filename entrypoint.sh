a42fbe9b7b8ad4fe4fa57763cec599f923d31e63
jadutter <4691511+jadutter@users.noreply.github.com>
2021-02-22 18:20:10 -0500

    modified entrypoint to properly quote output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # print the output and errors, but also send them to files to be read later
 ((eval "${INPUT_CMD}" | tee /tmp/output && echo "${PIPESTATUS[0]}" > /tmp/exit_code) 3>&1 1>&2 2>&3 | tee /tmp/error)
-echo "::set-output name=stderr::$(cat /tmp/error)"
-echo "::set-output name=stdout::$(cat /tmp/output)"
+echo "::set-output name=stderr::\"$(cat /tmp/error | sed -E 's/"/\\"/g')\""
+echo "::set-output name=stdout::\"$(cat /tmp/output | sed -E 's/"/\\"/g')\""
 echo "::set-output name=exit_code::$(cat /tmp/exit_code)"
 exit "$(cat /tmp/exit_code)"

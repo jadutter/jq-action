5029fb74c90aba499cf0f43f04a3dfda748622a2
jadutter <4691511+jadutter@users.noreply.github.com>
2021-03-01 21:50:50 -0500

    fixed it so entrypoint can handle multiline output, and test-all is able to verify mulit-line outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 # print the output and errors, but also send them to files to be read later
-sed_command='
+sed_command="$( 
+    echo -e "
     :nextLine;
-    {
-        # add the next line to the pattern space
-        N;
-    }
     # if not the end of the file
-    $!{
-        # grab another line
-        b nextLine;
+    \$!{
+      # add the next line to the pattern space
+      N;
+      # grab another line
+      b nextLine;
     }
-    # esape newline characters
-    s/\n/%0A/g;
-    s/\r/%0D/g;
-    # escape characters that would interfere with the `echo ":::set-output...`
-    s/("|\\|\$)/\\\1/g;
+    # escape backslash characters
+    s/\\\\\\/\\\\\\\\\\\\\\/g;
+
     # escape percentage sign
     s/%/%25/g;
-    # escape whitespace 
-    s/	/%09/g;
-    s/ /%20/g;
-'
+
+    # esape newline characters
+    s/\\\n/%0A/g;
+    s/\\\r/%0D/g;
+
+    # escape double quotes
+    s/\\\"/\\\\\\\\\\\"/g;
+
+    # escape dollar sign
+    s/\\\\\$/\\\\\\\\\$/g;
+    "
+)"
 ((eval "${INPUT_CMD}" | tee /tmp/output && echo "${PIPESTATUS[0]}" > /tmp/exit_code) 3>&1 1>&2 2>&3 | tee /tmp/error)
 echo "::set-output name=stderr::$(cat /tmp/error | sed -E "${sed_command}" )"
 echo "::set-output name=stdout::$(cat /tmp/output | sed -E "${sed_command}" )"

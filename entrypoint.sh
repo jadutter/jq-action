acb657ea4bad4299ec97c9ed57d8e134631eafe8
jadutter <4691511+jadutter@users.noreply.github.com>
2023-12-21 15:43:41 -0500

    updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,34 +1,7 @@
 #!/bin/bash
 # print the output and errors, but also send them to files to be read later
-sed_command="$( 
-    echo -e "
-    :nextLine;
-    # if not the end of the file
-    \$!{
-      # add the next line to the pattern space
-      N;
-      # grab another line
-      b nextLine;
-    }
-    # escape backslash characters
-    s/\\\\\\/\\\\\\\\\\\\\\/g;
-
-    # escape percentage sign
-    s/%/%25/g;
-
-    # esape newline characters
-    s/\\\n/%0A/g;
-    s/\\\r/%0D/g;
-
-    # escape double quotes
-    s/\\\"/\\\\\\\\\\\"/g;
-
-    # escape dollar sign
-    s/\\\\\$/\\\\\\\\\$/g;
-    "
-)"
 ((eval "${INPUT_CMD}" | tee /tmp/output && echo "${PIPESTATUS[0]}" > /tmp/exit_code) 3>&1 1>&2 2>&3 | tee /tmp/error)
-echo "::set-output name=stderr::$(cat /tmp/error | sed -E "${sed_command}" )"
-echo "::set-output name=stdout::$(cat /tmp/output | sed -E "${sed_command}" )"
-echo "::set-output name=exit_code::$(cat /tmp/exit_code)"
+echo "exit_code=$(cat /tmp/exit_code)"  >> $GITHUB_OUTPUT
+echo "stderr=$(cat /tmp/error | jq -sRr @uri )"  >> $GITHUB_OUTPUT
+echo "stdout=$(cat /tmp/output | jq -sRr @uri )"  >> $GITHUB_OUTPUT
 exit "$(cat /tmp/exit_code)"

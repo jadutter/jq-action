b0a24c88fd79493bb99725a590e595824fc15188
jadutter <4691511+jadutter@users.noreply.github.com>
2023-12-21 15:33:43 -0500

    updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # print the output and errors, but also send them to files to be read later
-((eval $INPUT_CMD | tee /tmp/output) 3>&1 1>&2 2>&3 | tee /tmp/error)
+((eval $INPUT_CMD | tee /tmp/out) 3>&1 1>&2 2>&3 | tee /tmp/err)
 EXIT_CODE="$?"
-echo "::set-output name=stderr::$(cat /tmp/error)"
-echo "::set-output name=stdout::$(cat /tmp/output)"
-echo "::set-output name=exit_code::$EXIT_CODE"
+echo "exit_code=$EXIT_CODE" >> $GITHUB_OUTPUT
+echo "stderr=$( cat /tmp/err | jq -sRr @uri )" >> $GITHUB_OUTPUT
+echo "stdout=$( cat /tmp/out | jq -sRr @uri )" >> $GITHUB_OUTPUT
+

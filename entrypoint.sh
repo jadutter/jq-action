b8a76cb367a6f4c4b37da468ea578a9be31867ad
jadutter <4691511+jadutter@users.noreply.github.com>
2023-12-21 19:05:00 -0500

    updated workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 # print the output and errors, but also send them to files to be read later
-((eval "${INPUT_CMD}" | tee /tmp/output && echo "${PIPESTATUS[0]}" > /tmp/exit_code) 3>&1 1>&2 2>&3 | tee /tmp/error)
+echo "${INPUT_CMD}">/tmp/input
+((eval "$(cat /tmp/input)" | tee /tmp/output && echo "${PIPESTATUS[0]}" > /tmp/exit_code) 3>&1 1>&2 2>&3 | tee /tmp/error)
 echo "exit_code=$(cat /tmp/exit_code)"  >> $GITHUB_OUTPUT
-echo "stderr=$(cat /tmp/error | ./encode.sh )"  >> $GITHUB_OUTPUT
-echo "stdout=$(cat /tmp/output | ./encode.sh )"  >> $GITHUB_OUTPUT
+(
+    echo 'stderr<<EOF'
+    cat /tmp/error
+    echo 'EOF'
+)  >> $GITHUB_OUTPUT
+(
+    echo 'stdout<<EOF'
+    cat /tmp/output
+    echo 'EOF'
+)  >> $GITHUB_OUTPUT
 exit "$(cat /tmp/exit_code)"

214164e852667f6b9b863b92a1a63da039f76089
jadutter <4691511+jadutter@users.noreply.github.com>
2025-03-24 18:02:52 -0400

    updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # print the output and errors, but also send them to files to be read later
-echo 1>/tmp/exit_code
+echo "1">/tmp/exit_code
 echo "${INPUT_CMD}" >/tmp/input
 
 ((eval "$(cat /tmp/input)" 1>/tmp/output 2>/tmp/error; echo "$?" > /tmp/exit_code) )
@@ -21,6 +21,10 @@ if [[ ! "$(cat /tmp/error | tail -c 1)" == "$(printf '\n')" ]]; then
     printf '\n' >>$GITHUB_OUTPUT
 fi
 printf 'EOF_STDERR\n' >>$GITHUB_OUTPUT
+
+if [[ "$(cat /tmp/exit_code|tr -dc '[:digit:]')" -lt 1 ]]; then
+    echo "1">/tmp/exit_code
+fi
 
 echo "exit_code=$(cat /tmp/exit_code)" >> $GITHUB_OUTPUT
 exit "$(cat /tmp/exit_code)"

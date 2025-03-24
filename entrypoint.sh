22677b9498dd7bb24108b53a1f182c3a7a9831fd
jadutter <4691511+jadutter@users.noreply.github.com>
2025-03-24 17:41:57 -0400

    updated workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,17 @@ cat /tmp/error >&2
 
 echo 'stdout<<EOF_STDOUT'>>$GITHUB_OUTPUT
 cat /tmp/output >>$GITHUB_OUTPUT
-printf '\nEOF_STDOUT\n' >>$GITHUB_OUTPUT
+if [[ ! "$(cat /tmp/output | tail -b 1)" == "$(printf '\n')" ]]; then 
+    printf '\n' >>$GITHUB_OUTPUT
+fi
+printf 'EOF_STDOUT\n' >>$GITHUB_OUTPUT
 
 echo 'stderr<<EOF_STDERR'>>$GITHUB_OUTPUT
 cat /tmp/error >>$GITHUB_OUTPUT
-printf '\nEOF_STDERR\n' >>$GITHUB_OUTPUT
+if [[ ! "$(cat /tmp/error | tail -b 1)" == "$(printf '\n')" ]]; then 
+    printf '\n' >>$GITHUB_OUTPUT
+fi
+printf 'EOF_STDERR\n' >>$GITHUB_OUTPUT
 
 echo "exit_code=$(cat /tmp/exit_code)" >> $GITHUB_OUTPUT
 exit "$(cat /tmp/exit_code)"

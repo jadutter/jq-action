a088df6969a54339ecdbf33e630feedc3ca3a75c
jadutter <4691511+jadutter@users.noreply.github.com>
2025-03-24 17:43:05 -0400

    fix: updated entrypoint (#8)
    * updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 # print the output and errors, but also send them to files to be read later
-# echo "${INPUT_CMD}" | sed -E 's,([$]),\\$1,g;' >/tmp/input
+
 echo "${INPUT_CMD}" >/tmp/input
-((eval "$(cat /tmp/input)" | tee /tmp/output && echo "${PIPESTATUS[0]}" > /tmp/exit_code) 3>&1 1>&2 2>&3 | tee /tmp/error)
-echo "exit_code=$(cat /tmp/exit_code)"  >> $GITHUB_OUTPUT
-(
-    echo 'stderr<<EOF'
-    cat /tmp/error
-    echo 'EOF'
-)  >> $GITHUB_OUTPUT
-(
-    echo 'stdout<<EOF'
-    cat /tmp/output
-    echo 'EOF'
-)  >> $GITHUB_OUTPUT
+
+((eval "$(cat /tmp/input)" 1>/tmp/output 2>/tmp/error; echo "$?" > /tmp/exit_code) )
+
+cat /tmp/output
+cat /tmp/error >&2
+
+echo 'stdout<<EOF_STDOUT'>>$GITHUB_OUTPUT
+cat /tmp/output >>$GITHUB_OUTPUT
+if [[ ! "$(cat /tmp/output | tail -b 1)" == "$(printf '\n')" ]]; then 
+    printf '\n' >>$GITHUB_OUTPUT
+fi
+printf 'EOF_STDOUT\n' >>$GITHUB_OUTPUT
+
+echo 'stderr<<EOF_STDERR'>>$GITHUB_OUTPUT
+cat /tmp/error >>$GITHUB_OUTPUT
+if [[ ! "$(cat /tmp/error | tail -b 1)" == "$(printf '\n')" ]]; then 
+    printf '\n' >>$GITHUB_OUTPUT
+fi
+printf 'EOF_STDERR\n' >>$GITHUB_OUTPUT
+
+echo "exit_code=$(cat /tmp/exit_code)" >> $GITHUB_OUTPUT
 exit "$(cat /tmp/exit_code)"
